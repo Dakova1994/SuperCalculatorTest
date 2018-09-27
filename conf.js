98206564213712c8b79b73c9952e1d6b2bb2f4c0
Dakova1994 <cejba.patryk@gmail.com>
2018-09-27 11:53:41 +0200

    use 2 browser for testing

--- conf.js
+++ conf.js
@@ -1,8 +1,10 @@
 exports.config = {
-    framework: 'jasmine',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['spec.js'],
-    capabilities: {
-      browserName: 'firefox'
-    }
-  }
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec.js'],
+  multiCapabilities: [{
+    browserName: 'firefox'
+  }, {
+    browserName: 'chrome'
+  }]
+}
